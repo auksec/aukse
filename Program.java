4859fe9f20d8a320fd4197eab5fa116ab715dc4f
auksec <caukse@gmail.com>
2017-01-10 17:48:55 +0200

    pridetas nuskaitymas is klaviaturos

--- Program.java
+++ Program.java
@@ -4,8 +4,11 @@ public class Program
 {
 	public static void main (String[] arg)
 	{
-	Scanner scanner = new Scanner(System.in);		
 	System.out.println("Hello");
 	System.out.println("Happy New Year");
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		System.out.println(a);
+	
 	}
 }
